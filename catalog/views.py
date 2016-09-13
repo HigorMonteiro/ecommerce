ff3c9ee99f526ef3740749c9825ad725c8d473cb
higormonteiro <higor.tecinfor@gmail.com>
2016-09-12 22:25:16 -0300

    criando templates & listagem de produtos/categoria

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Product, Category
 # Create your views here.
+
+
+def product_list(request):
+    context = {
+        "product_list": Product.objects.all()
+    }
+
+    return render(request, 'catalog/product_list.html', context)
+
+def category(request, slug):
+    category = Category.objects.get(slug=slug)
+    context = {
+        'current_category': category,
+        'product_list': Product.objects.filter(category=category),
+    }
+    return render(request, 'catalog/category.html', context)
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,14 +2,15 @@ from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+
 from core import views
+from catalog import views as views_catalog
 admin.autodiscover()
 
 urlpatterns = [
-
     url(r'^$', views.index, name='index'),
     url(r'^contato/$', views.contact, name='contact'),
     url(r'^produto/$', views.product, name='product'),
-    url(r'^produtos/$', views.product_list, name='product_list'),
+    url(r'^produtos/', include('catalog.urls', namespace='catalog')),
     url(r'^admin/', include(admin.site.urls)),
 ]
