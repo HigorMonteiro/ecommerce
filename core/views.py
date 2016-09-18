76b88c676ed0e1a1f0e1085e84a996deaac2eccb
higormonteiro <higor.tecinfor@gmail.com>
2016-09-18 05:36:02 -0300

    formulario de contato

--- core/views.py
+++ core/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import render
-# Create your views here.
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
+from django.views.generic import View, TemplateView
+
+from .forms import ContactForm
 
 
-def index(request):
-    return render(request, 'index.html')
+class IndexView(TemplateView):
 
+    template_name = 'index.html'
+
+
+index = IndexView.as_view()
 
 def contact(request):
-    return render(request, 'contact.html')
-
-
-def product(request):
-    return render(request, 'product.html')
+    success = False
+    form = ContactForm(request.POST or None)
+    if form.is_valid():
+        form.send_mail()
+        success = True
+    context = {
+        'form': form,
+        'success': success
+    }
+    return render(request, 'contact.html', context)
+    
